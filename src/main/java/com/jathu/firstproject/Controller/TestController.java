69e5fda398ff146f37151538bac9e28c4f20aae8
Yathushana <thirumaljathushana128@gmail.com>
2024-08-09 20:00:57 +0530

    first commit

--- src/main/java/com/jathu/firstproject/Controller/TestController.java
+++ src/main/java/com/jathu/firstproject/Controller/TestController.java
@@ -1,9 +1,6 @@
 package com.jathu.firstproject.Controller;
-
-
 import com.jathu.firstproject.Service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
